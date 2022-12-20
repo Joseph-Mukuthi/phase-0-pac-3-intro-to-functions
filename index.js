9406f57a4cf63e5808da48944f2b285d314b6102
Joseph-Mukuthi <joseph.mukuthi@student.moringaschool.com>
2022-12-20 12:15:17 +0300

    Add functions to say hello to different names

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Follow along with the examples here
+function sayHello(){
+    console.log('Hello, World!');
+}
+sayHello();
+
+function sayHelloToGuadalupe(){
+    console.log('Hello, Guadalupe!');
+}
+sayHelloToGuadalupe();
+
+function sayHelloToLiz(){
+    console.log('Hello, Liz');
+}
+sayHelloToLiz();
+
+function sayHelloToSamip(){
+    console.log('Hello, Samip!');
+}
+sayHelloToSamip();
+
